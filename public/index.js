0dc0f77d0e1e7366fcb91798a7caab92f434be21
jayrgee <jayrgee@fastmail.fm>
2021-12-13 00:03:43 +1100

    cleanup model

--- public/index.js
+++ public/index.js
@@ -1,26 +1,24 @@
-// init application namespace
-const APPNS = {};
+// init model in app namespace
+const APP_MODEL = {};
 
-(function (app) {
+(function (model) {
   fetch('films.json')
     .then((response) => response.json())
     .then((data) => {
-      //   console.log(data);
-      app.data = data;
-      app.films = ko.observableArray(data);
+      model.data = data;
 
-      app.collections = ko.observableArray([
+      model.collections = ko.observableArray([
         'alpha',
         'bravo',
         'charlie',
         'delta',
         'echo',
         'foxtrot',
-        'golf'
+        'golf',
       ]);
 
       const genres = data.flatMap((i) => i.Genre.split(', '));
-      app.genres = ko.observableArray([
+      model.genres = ko.observableArray([
         '',
         ...genres
           .filter((item, index) => genres.indexOf(item) === index)
@@ -28,30 +26,31 @@ const APPNS = {};
       ]);
 
       const countries = data.flatMap((i) => i.Country.split(', '));
-      app.countries = ko.observableArray([
+      model.countries = ko.observableArray([
         '',
         ...countries
           .filter((item, index) => countries.indexOf(item) === index)
           .sort(),
       ]);
 
-      app.genreToShow = ko.observable('');
-      app.countryToShow = ko.observable('');
+      model.genreToShow = ko.observable('');
+      model.countryToShow = ko.observable('');
 
-      app.filmsToShow = ko.computed(function () {
-        // Represents a filtered list of films
-        // i.e., only those matching the "genreToShow" condition
-        const genre = app.genreToShow();
-        const country = app.countryToShow();
-        // if (genre === '' && country === '') return app.films();
-        return ko.utils.arrayFilter(app.films(), function (film) {
+      model.filmsToShow = ko.computed(function () {
+        // Returns a filtered list of films
+
+        // get selection criteria
+        const genre = model.genreToShow();
+        const country = model.countryToShow();
+
+        return ko.utils.arrayFilter(data, function (film) {
           return (
             (film.Genre.indexOf(genre) >= 0 || genre === '') &&
             (film.Country.indexOf(country) >= 0 || country === '')
           );
         });
-      }, app);
+      }, model);
 
-      ko.applyBindings(app);
+      ko.applyBindings(model);
     });
-})(APPNS);
+})(APP_MODEL);
